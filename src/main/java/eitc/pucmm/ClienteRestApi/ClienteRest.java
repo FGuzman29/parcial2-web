45fdc702572ed67bbeca09c59b8b29a690c2c51c
PR0C3SS <20180767@CE.PUCMM.EDU.DO>
2021-08-25 21:54:09 -0400

    UPDATE REST-API 100%

--- src/main/java/eitc/pucmm/ClienteRestApi/ClienteRest.java
+++ src/main/java/eitc/pucmm/ClienteRestApi/ClienteRest.java
@@ -15,9 +15,7 @@ import java.util.Scanner;
 public class ClienteRest {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         String url;
         String usuario;
         String password;
